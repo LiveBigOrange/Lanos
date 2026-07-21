8b80329e9c10ca1f54f3dd9c959e6690b785fc72
yzn <yzn@lanos.local>
2026-07-22 00:16:55 +0800

    ci: temporarily disable macOS build (runner queuing)

--- ui/linux/flutter/generated_plugin_registrant.cc
+++ ui/linux/flutter/generated_plugin_registrant.cc
@@ -6,22 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_selector_linux/file_selector_plugin.h>
-#include <local_notifier/local_notifier_plugin.h>
-#include <system_tray/system_tray_plugin.h>
-#include <tray_manager/tray_manager_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
-  file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
-  g_autoptr(FlPluginRegistrar) local_notifier_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "LocalNotifierPlugin");
-  local_notifier_plugin_register_with_registrar(local_notifier_registrar);
-  g_autoptr(FlPluginRegistrar) system_tray_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "SystemTrayPlugin");
-  system_tray_plugin_register_with_registrar(system_tray_registrar);
-  g_autoptr(FlPluginRegistrar) tray_manager_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "TrayManagerPlugin");
-  tray_manager_plugin_register_with_registrar(tray_manager_registrar);
 }
--- ui/linux/flutter/generated_plugins.cmake
+++ ui/linux/flutter/generated_plugins.cmake
@@ -3,14 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  file_selector_linux
-  local_notifier
-  system_tray
-  tray_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
